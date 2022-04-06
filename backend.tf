22bc746921b6e193609de1a207b07ab3e2c69877
khasimvalishaik <33258517+khasimvalishaik@users.noreply.github.com>
2022-04-06 18:11:36 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "khasimmydev-tf-state-bucket"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
