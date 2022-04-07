fab39399d274c48c2eed4ddb65f1cd0c1d665f2f
khasimvalishaik <33258517+khasimvalishaik@users.noreply.github.com>
2022-04-07 16:48:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "terraformstatefile-mumbai"
+    bucket = "khasimmydev-tf-state-bucket"
     key = "main"
     region = "ap-south-1"
-    dynamodb_table = "terraformstatetable"
+    dynamodb_table = "khasim-dynamodb-table"
   }
 }
 
